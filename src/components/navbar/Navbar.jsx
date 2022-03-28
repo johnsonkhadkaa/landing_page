93423e8e6bcf0dd66432768346deac9029d9e96f
JOHNSON KHADKA <er.johnsonkhadka@gmail.com>
2022-03-28 08:29:43 +0545

    landing page

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './navbar.css';
+import {RiMenu3Line , RiCloseLine } from 'react-icons/ri';
+
+const Navbar = () => {
+  return (
+    <div className=''>
+      
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div>
+        Header
+
+    </div>
+  )
+}
+
+export default Header
